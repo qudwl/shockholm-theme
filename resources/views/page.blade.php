28cdacb8517463140c6244a7f96c9c81d95c1e32
Byeongji Lim <byeongjilim@gmail.com>
2025-09-23 18:28:11 +0200

    changed menu to sans serif

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -5,7 +5,7 @@
 <!-- @include('partials.page-header') -->
 @if (!is_front_page() && !is_home() && has_post_thumbnail())
   <section id="landing-section" class="page-landing-section"
-    style="background-image: url('{{ get_the_post_thumbnail_url() }}')">
+    style="background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url('{{ get_the_post_thumbnail_url() }}')">
     <h1 class="entry-title">{{ get_the_title() }}</h1>
     <p class="entry-excerpt">{{ get_the_excerpt() }}</p>
   </section>
